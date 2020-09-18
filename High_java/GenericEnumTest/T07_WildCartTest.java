162f5b842a32a30acfab52c389f13632f71008e2
mo_on <45116091+answjd5794@users.noreply.github.com>
2020-09-18 15:00:26 +0900

    [Final] WildCard와 Enum

--- High_java/GenericEnumTest/T07_WildCartTest.java
+++ High_java/GenericEnumTest/T07_WildCartTest.java
@@ -1,7 +1,76 @@
 package kr.or.ddit.basic;
 
+import java.util.Arrays;
+
 public class T07_WildCartTest {
 
+	/**
+	 * 모든 과정을 등록하는 메서드
+	 * 
+	 * @method registerCourse
+	 * @return void
+	 * @author 강문정
+	 */
+	public static void registerCourse(Course<?> course) {
+		System.out.println(course.getName() + "수강생 : " + Arrays.toString(course.getStudents()));
+
+	}
+
+	/**
+	 * 학생 과정 등록 메서드 (학생만 받겠다)
+	 * 
+	 * @method registerCourseStudent
+	 * @return void
+	 * @author 강문정
+	 */
+	public static void registerCourseStudent(Course<? extends Student> course) {
+		System.out.println(course.getName() + "수강생 : " + Arrays.toString(course.getStudents()));
+	}
+
+	/**
+	 * 일반인 과정 등록 메서드
+	 * 
+	 * @method registerCourseWorker
+	 * @param course
+	 * @return void
+	 * @author 강문정
+	 */
+	public static void registerCourseWorker(Course<? super Worker> course) {
+		System.out.println(course.getName() + "수강생 : " + Arrays.toString(course.getStudents()));
+	}
+
+	public static void main(String[] args) {
+		Course<Person> personCourse = new Course<>("일반인 과정", 5);
+		personCourse.add(new Person("일반인 1"));
+		personCourse.add(new Worker("직장인 1"));
+		personCourse.add(new Student("학생 1"));
+		personCourse.add(new HighSchool("학생 1"));
+
+		Course<Worker> workerCourse = new Course<>("직장인 과정", 5);
+		workerCourse.add(new Worker("직장인1"));
+
+		Course<Student> studentCourse = new Course<>("학생 과정", 5);
+		studentCourse.add(new Student("학생1"));
+		studentCourse.add(new Student("고등학생1"));
+
+		Course<HighSchool> highStudentCourse = new Course<>("고등 학생 과정", 5);
+		highStudentCourse.add("고등학생1");
+
+		registerCourse(personCourse);
+		registerCourse(workerCourse);
+		registerCourse(studentCourse);
+		registerCourse(highStudentCourse);
+		System.out.println("============================================");
+		// registerCourseStudent(personCourse);
+		// registerCourseStudent(workerCourse);
+		registerCourseStudent(studentCourse);
+		registerCourseStudent(highStudentCourse);
+		System.out.println("============================================");
+		registerCourseWorker(personCourse);
+		registerCourseWorker(workerCourse);
+		// registerCourseWorker(studentCourse);
+		// registerCourseWorker(highStudentCourse);
+	}
 }
 
 // 일반인
@@ -52,19 +121,24 @@ class Course<T> {
 		this.name = name;
 
 		// 타입 파라미터로 배열을 생성시 오브젝트 배열을 생성 후, 타입 파라미터 배열로 캐스팅 해야함.
-		students = (T[])(new Object[capacity]);
+		students = (T[]) (new Object[capacity]);
 	}
-	
-	
+
+	public void add(String string) {
+		// TODO Auto-generated method stub
+
+	}
+
 	// 수강생 조회
 	public String getName() {
 		return name;
 	}
+
 	// 코스명 조회
 	public T[] getStudents() {
 		return students;
 	}
-	
+
 	// 수강생 등록
 	public void add(T t) {
 		for (int i = 0; i < students.length; i++) {
